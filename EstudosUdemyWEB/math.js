8f96ee1393a19bb41f1470ca09856c15784712ae
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-10-26 18:54:59 -0300

    estudando sobre math, objeto nativo de JS que contém funções e propriedades matemáticas

---
+++ EstudosUdemyWEB/math.js
@@ -0,0 +1,6 @@
+//Estudando sobre math em JavaScript
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2); //Math.PI para o valor de π (PI) e Math.pow para potência
+
+console.log(area);
+console.log(typeof Math); //Math é um objeto nativo no JS que contém propriedades e métodos para constantes e funções matemáticas
