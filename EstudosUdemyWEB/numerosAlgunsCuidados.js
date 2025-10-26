212c7635a43216fd9dbd6d9a356b4c1d72c5d57a
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-10-26 18:51:51 -0300

    estudando sobre as limitações dos números nos logs do javaScript e cuidados

---
+++ EstudosUdemyWEB/numerosAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0); // Divisão por zero retorna Infinity, que se trata de um valor numérico especial do JavaScript
+console.log("10" / 2); // O JavaScript converte a string "10" para número automaticamente
+console.log("Show!" * 2); // Operação inválida retorna NaN (Not a Number)
+console.log(0.1 + 0.7); // Problemas de precisão com números de ponto flutuante
+console.log((10.345).toFixed(2)); // Usando toFixed para limitar casas decimais
