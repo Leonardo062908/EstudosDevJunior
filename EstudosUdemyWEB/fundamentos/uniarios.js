bd32266f4b73f6ff800e265012b573ed7488e719
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-11-17 22:53:00 -0300

    feat: aprendendo sobre operadores unários

---
+++ EstudosUdemyWEB/fundamentos/uniarios.js
@@ -0,0 +1,13 @@
+//Aprendendo a usar operadores unários em JavaScript
+let num1 = 1;
+let num2 = 2;
+
+num1++; //Operador unário de incremento, adiciona 1 ao valor de num1
+console.log(num1); //Exibe o valor atualizado de num1 (2)
+--num1; //Operador unário de decremento, subtrai 1 do valor de num1
+console.log(num1); //Exibe o valor atualizado de num1 (1)
+
+console.log(++num1 === num2--); //Compara se o valor incrementado de num1 é igual ao valor de num2 antes do decremento
+console.log(num1 === num2); //Compara se os valores atuais de num1 e num2 são iguais
+
+//Deixar código simples e legível é sempre a melhor prática!
